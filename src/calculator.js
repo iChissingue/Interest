3cc3fe3c8ad817387e242916c3362ac1ced6dc5a
Inacio Antonio Chissingue <ichissingue@outlook.com>
2026-07-26 13:04:16 +0200

    first commit

---
+++ src/calculator.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+const Calculate = () => {
+
+    const [loan, setLoan] = useState(0)
+    const [delayDays, setDelayDays] = useState(0)
+
+    const interest = parseInt(loan) * 0.1
+    const DelayInterest = parseInt(interest) / 30 * delayDays
+
+    return (
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+            <div style={{ display: 'flex', flexDirection: 'column', width: 200 }}>
+            <h3>Calculadora de juros mensais e de mora</h3>
+                <label>Valor de emprestimo:</label>
+                <input type="number" onChange={(e) => setLoan(e.target.value)} />
+                <p>Juros: {interest.toFixed(2)}MT</p>
+                 <label>Dias de Atraso:</label>
+                <input type="number" onChange={(e) => setDelayDays(e.target.value)} />
+                <p>Juros de Mora: {DelayInterest.toFixed(2)}MT</p>
+                <p>Total de Juros: {Number(DelayInterest.toFixed(2) + interest.toFixed(2))}MT</p>
+            </div>
+        </div>
+    )
+}
+
+export default Calculate;
